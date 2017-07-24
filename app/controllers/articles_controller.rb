4cad95cb3fc03359b690f0d517f7c00337087c87
lea <lea.giroud@gmail.com>
2017-07-24 15:01:24 +0200

    form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,38 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id]) 
+  end
+  def new
+  	@article = Article.new
+  end
+  def create
+  	@article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' Créer!"
+    redirect_to article_path(@article)
+  end
+
+
+  def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+	flash.notice = "Article '#{@article.title}' Supprimer!"
+    redirect_to article_path
+  end
+
+  def edit
+      @article = Article.find(params[:id])
+      flash.notice = "Article '#{@article.title}' Edit !"
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+     flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
 end
